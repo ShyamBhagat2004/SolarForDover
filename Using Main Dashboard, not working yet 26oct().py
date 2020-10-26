e7feed8c26ee578a4ef96f77d0d02efaaad95782
wearethedropperz <67416253+wearethedropperz@users.noreply.github.com>
2020-10-26 22:06:10 +0800

    Made some progress on Beautiful Soup. Need to figure out how to print specific element

--- Using Main Dashboard, not working yet 26oct().py
+++ Using Main Dashboard, not working yet 26oct().py
@@ -1,3 +1,13 @@
 import requests
+from bs4 import BeautifulSoup
 
-Tutorial = "https://www.dataquest.io/blog/web-scraping-tutorial-python/""
+Tutorial = "https://www.dataquest.io/blog/web-scraping-tutorial-python/"
+
+
+page = requests.get("https://www.sunnyportal.com/Templates/PublicPage.aspx?page=1169a2ff-8f51-4ea9-ba72-316009593c62")
+soup = BeautifulSoup(page.content, 'html.parser')
+#print(soup.prettify())
+
+print([type(item) for item in list(soup.children)])
+html = list(soup.children)[8]
+print(list(html.children))
