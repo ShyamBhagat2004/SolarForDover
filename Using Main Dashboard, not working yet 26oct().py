2d70c990114980cdac196a6341d603ba47a6d9b9
Shyam <67416253+wearethedropperz@users.noreply.github.com>
2020-10-27 09:43:46 +0800

    Adjusted for Macintosh. Created virtual environment and reinstalled python3 using brew

--- Using Main Dashboard, not working yet 26oct().py
+++ Using Main Dashboard, not working yet 26oct().py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 Tutorial = "https://www.dataquest.io/blog/web-scraping-tutorial-python/"
-
+VirtualEnvironment = "https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/"
 
 page = requests.get("https://www.sunnyportal.com/Templates/PublicPage.aspx?page=1169a2ff-8f51-4ea9-ba72-316009593c62")
 soup = BeautifulSoup(page.content, 'html.parser')
