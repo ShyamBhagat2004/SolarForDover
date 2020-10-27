c0eea07b82010e3da55628a7efff4006ae1f5269
Shyam <67416253+wearethedropperz@users.noreply.github.com>
2020-10-27 15:35:44 +0800

    Finished Combining both Plants and have the value

--- dashboard_scraper.py
+++ dashboard_scraper.py
@@ -1,19 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+import math
 Tutorial = "https://www.dataquest.io/blog/web-scraping-tutorial-python/"
 VirtualEnvironment = "https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/"
 
-page = requests.get("https://www.sunnyportal.com/Templates/PublicPage.aspx?page=1169a2ff-8f51-4ea9-ba72-316009593c62")
+page = requests.get("https://bit.ly/37Gvm9v")
 soup = BeautifulSoup(page.content, 'html.parser')
 nig = (soup.find(id="ctl00_ContentPlaceHolder1_PublicPagePlaceholder1_PageUserControl_ctl00_PublicPageLoadFixPage_energyYieldWidget_energyYieldValue"))
+
+pagePoolPanels = requests.get("https://www.sunnyportal.com/Templates/PublicPage.aspx?page=e30bffa6-04b5-45e7-b9d5-bdd17fc918cd")
+soupPool = BeautifulSoup(pagePoolPanels.content, 'html.parser')
 #print(nig.get_text())
 
 def get_current_pv_value():
-    pv_current = soup.find("span", attrs = {"class" : "mainValueAmount"})
-    value = int(pv_current['data-value'])
-    print(value)
-
+    pv_current_junior = soup.find("span", attrs = {"class" : "mainValueAmount"})
+    pv_current_pool = soupPool.find("span", attrs = {"class" : "mainValueAmount"})
+    value_junior_current_int = int(pv_current_junior['data-value'])
+    value_pool_current_int = int(pv_current_pool['data-value'])
+    value_total_current_int = value_junior_current_int + value_pool_current_int
+    value_total_current_int = value_total_current_int / 1000
+    finalCurrentPower = math.trunc(value_total_current_int)
+    
     
 
 get_current_pv_value()
