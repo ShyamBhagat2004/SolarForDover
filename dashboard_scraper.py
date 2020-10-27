bb1fcce1ef35df802a09ea301cdc10ab9975e622
Shyam <67416253+wearethedropperz@users.noreply.github.com>
2020-10-27 15:15:00 +0800

    Update dashboard_scraper.py

--- dashboard_scraper.py
+++ dashboard_scraper.py
@@ -7,14 +7,13 @@ VirtualEnvironment = "https://packaging.python.org/guides/installing-using-pip-a
 page = requests.get("https://www.sunnyportal.com/Templates/PublicPage.aspx?page=1169a2ff-8f51-4ea9-ba72-316009593c62")
 soup = BeautifulSoup(page.content, 'html.parser')
 nig = (soup.find(id="ctl00_ContentPlaceHolder1_PublicPagePlaceholder1_PageUserControl_ctl00_PublicPageLoadFixPage_energyYieldWidget_energyYieldValue"))
-print(nig.get_text())
+#print(nig.get_text())
 
 def get_current_pv_value():
-    
     pv_current = soup.find("span", attrs = {"class" : "mainValueAmount"})
     value = int(pv_current['data-value'])
     print(value)
-    print(value + 1)
+
     
 
 get_current_pv_value()
