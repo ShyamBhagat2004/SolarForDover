f4fe5c3405aff3665515d213addf84d0f9ad09be
wearethedropperz <67416253+wearethedropperz@users.noreply.github.com>
2020-10-28 21:33:31 +0800

    dweeter working

--- dashboard_scraper.py
+++ dashboard_scraper.py
@@ -26,15 +26,25 @@ def get_current_pv_value():
     get_current_pv_value.final = finalCurrentPower
 def get_current_pv_value_and_dweet():
     import dweepy
+    page = requests.get("https://bit.ly/37Gvm9v")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    pagePoolPanels = requests.get("https://www.sunnyportal.com/Templates/PublicPage.aspx?page=e30bffa6-04b5-45e7-b9d5-bdd17fc918cd")
+    soupPool = BeautifulSoup(pagePoolPanels.content, 'html.parser')
+    
     pv_current_junior = soup.find("span", attrs = {"class" : "mainValueAmount"})
     pv_current_pool = soupPool.find("span", attrs = {"class" : "mainValueAmount"})
     value_junior_current_int = int(pv_current_junior['data-value'])
-    value_pool_current_int = int(pv_current_pool['data-value'])
+    value_pool_current_int = int(pv_current_pool['data-value']) / 1000
     value_total_current_int = value_junior_current_int + value_pool_current_int
+    print(f"The current power in watts not kilowatts is {value_total_current_int }") 
     value_total_current_int = value_total_current_int / 1000
     finalCurrentPower = math.trunc(value_total_current_int)
     get_current_pv_value.final = finalCurrentPower
-    dweepy.dweet_for('shyam__7', {pv_current_pool['data-timestamp'] : finalCurrentPower})
+    print(f"The current power combined is {finalCurrentPower}")
+    if value_total_current_int * 1000 < 2000:
+        dweepy.dweet_for('shyam__7', {pv_current_pool['data-timestamp'] : finalCurrentPower*1000})
+    else:
+        dweepy.dweet_for('shyam__7', {pv_current_pool['data-timestamp'] : finalCurrentPower})
 
 
     
