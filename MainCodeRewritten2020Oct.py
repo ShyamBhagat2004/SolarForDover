baf245f7d8a85bc399e458f9a824211f19e8d533
Shyam <67416253+wearethedropperz@users.noreply.github.com>
2020-10-30 11:40:34 +0800

    Nothing

--- MainCodeRewritten2020Oct.py
+++ MainCodeRewritten2020Oct.py
@@ -14,9 +14,10 @@ count = 0
 
 
 while count != 1:
-    dashboard_scraper.pv_resetter_0()
     HourSourceCodeRecieve.recieveSourceFunction()
     HourSourceCodeRecieve.rip_lines_and_dweet()
     #dashboard_scraper.get_current_pv_value()
     dashboard_scraper.get_current_pv_value_and_dweet()
+    dashboard_scraper.pv_resetter_0()
+
     time.sleep(300)
