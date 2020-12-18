58e409c340f206f9257c20bceef7c8df6ae6ddf4
Shyam <67416253+wearethedropperz@users.noreply.github.com>
2020-12-18 10:28:13 +0800

    Added Except: continue to master file

--- MainCodeRewritten2020Oct.py
+++ MainCodeRewritten2020Oct.py
@@ -12,12 +12,16 @@ import dashboard_scraper
 import time
 count = 0
 
+try:
 
-while count != 1:
-    HourSourceCodeRecieve.recieveSourceFunction()
-    HourSourceCodeRecieve.rip_lines_and_dweet()
-    #dashboard_scraper.get_current_pv_value()
-    dashboard_scraper.get_current_pv_value_and_dweet()
-    dashboard_scraper.pv_resetter_0()
+    while count != 1:
+        HourSourceCodeRecieve.recieveSourceFunction()
+        HourSourceCodeRecieve.rip_lines_and_dweet()
+        #dashboard_scraper.get_current_pv_value()
+        dashboard_scraper.get_current_pv_value_and_dweet()
+        dashboard_scraper.pv_resetter_0()
 
-    time.sleep(300)
+        time.sleep(300)
+except:
+    continue
+        
