8815ac27cb4af8c852d775423fc9bf5c26afadc6
Shyam <67416253+wearethedropperz@users.noreply.github.com>
2020-12-28 18:38:53 +0800

    Changed 5 to 15 for PV Power Minimum

--- dashboard_scraper.py
+++ dashboard_scraper.py
@@ -47,9 +47,9 @@ def get_current_pv_value_and_dweet():
         dweepy.dweet_for('shyam__7', {'finalcurrentpower' : finalCurrentPower*1000})
     else:
         dweepy.dweet_for('shyam__7', {'finalCurrentPower' : finalCurrentPower})
-    if finalCurrentPower < 5:
+    if finalCurrentPower < 15:
         time.sleep(5)
-        dweepy.dweet_for('shyam__7', {'finalCurrentPower' : 5})
+        dweepy.dweet_for('shyam__7', {'finalCurrentPower' : 15})
 
 def pv_resetter_0():
     import dweepy
