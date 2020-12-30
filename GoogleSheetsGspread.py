4cc5982ec1274fe40df96e439693924d67c61175
Shyam <67416253+wearethedropperz@users.noreply.github.com>
2020-12-30 19:09:10 +0800

    n

--- GoogleSheetsGspread.py
+++ GoogleSheetsGspread.py
@@ -2,7 +2,6 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
 
-
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
