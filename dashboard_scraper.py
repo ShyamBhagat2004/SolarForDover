98efdff7f18b82871f941fb0f8afe6783597811f
Shyam <67416253+wearethedropperz@users.noreply.github.com>
2021-01-28 15:36:40 +0800

    Replaced Pool with High School Block (We weren't computing over 900 panels!)
    All pool values are actually artblk now. I know, but I'm on a bus right now and want to make sure it works when I get off.

--- dashboard_scraper.py
+++ dashboard_scraper.py
@@ -47,19 +47,20 @@ def get_current_pv_value_and_dweet():
     import time
     page = requests.get("https://bit.ly/37Gvm9v")
     soup = BeautifulSoup(page.content, 'html.parser')
-    pagePoolPanels = requests.get("https://www.sunnyportal.com/Templates/PublicPage.aspx?page=e30bffa6-04b5-45e7-b9d5-bdd17fc918cd")
+    pagePoolPanels = requests.get("https://bit.ly/highSchoolDoverSunny")
     soupPool = BeautifulSoup(pagePoolPanels.content, 'html.parser')
     
     pv_current_junior = soup.find("span", attrs = {"class" : "mainValueAmount"})
     pv_current_pool = soupPool.find("span", attrs = {"class" : "mainValueAmount"})
     value_junior_current_int = int(pv_current_junior['data-value'])
-    value_pool_current_int = int(pv_current_pool['data-value']) / 1000
+    value_pool_current_int = int(pv_current_pool['data-value'])
     value_total_current_int = value_junior_current_int + value_pool_current_int
     print(f"The current power in watts not kilowatts is {value_total_current_int }") 
     value_total_current_int = value_total_current_int / 1000
     finalCurrentPower = math.trunc(value_total_current_int)
     get_current_pv_value.final = finalCurrentPower
     print(f"The current power combined is {finalCurrentPower}")
+    
     """
     if value_total_current_int * 1000 < 2000:
         dweepy.dweet_for('shyam__7', {'finalcurrentpower' : finalCurrentPower*1000})
@@ -98,4 +99,5 @@ def pv_resetter_0():
     if now.hour == 23:
         """dweepy.dweet_for('shyam__7', {'finalCurrentPower' : 0})"""
         sheet.update_cell(23,2  , 0)
+
 
